135f85c851dd3f8a87792ceb4d8a95253b48419d
GoodLifeSeeker <vjunik88@gmail.com>
2023-03-02 15:42:14 +0300

    task2 before test

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -3,6 +3,68 @@
 class ConstructionStages
 {
 	private $db;
+	
+	// Function which validates the data
+	private function validateField($fieldName, $fieldValue)
+    	{
+            switch ($fieldName) {
+                
+                // Name validation
+                case 'name':
+                    if (strlen($fieldValue) > 5) {
+                        throw new Exception('Max chars amount is 255.');
+                    }
+                    break;
+                    
+                case 'start_date':
+		    if (!preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $fieldValue)) {
+    			throw new Exception('Invalid date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
+		    }
+		    break;
+                    
+                case 'end_date':
+                    if (!is_null($fieldValue)) {
+                        $timestamp = strtotime($fieldValue);
+                        if ($timestamp === false) {
+                            throw new Exception('Invalid end_date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
+                        }
+                        $startTimestamp = strtotime($this->startDate);
+                        if ($startTimestamp === false) {
+                            throw new Exception('Invalid start date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
+                        }
+                        if ($timestamp < $startTimestamp) {
+                            throw new Exception('End date cannot be earlier than start date');
+                        }
+                    }
+                    break;
+                    
+                case 'durationUnit':
+                    if (!in_array($fieldValue, ['HOURS', 'DAYS', 'WEEKS'])) {
+                        throw new Exception('Invalid duration unit. Use HOURS, DAYS, or WEEKS');
+                    }
+                    break;
+                    
+                case 'color':
+                    if (!is_null($fieldValue)) {
+                        if (!preg_match('/^#[0-9a-fA-F]{6}$/', $fieldValue)) {
+                            throw new Exception('Invalid color format. Use HEX format, e.g. #FF0000');
+                        }
+                    }
+                    break;
+                    
+                case 'externalId':
+                    if (!is_null($fieldValue) && strlen($fieldValue) > 255) {
+                        throw new Exception('External ID is too long');
+                    }
+                    break;
+                    
+                case 'status':
+                    if (!in_array($fieldValue, ['NEW', 'PLANNED', 'DELETED'])) {
+                        throw new Exception('Invalid status. Use NEW, PLANNED, or DELETED');
+                    }
+                    break;
+            }
+        }
 
 	public function __construct()
 	{
@@ -50,6 +112,10 @@ class ConstructionStages
 
 	public function post(ConstructionStagesCreate $data)
 	{
+	        foreach ($data as $fieldName => $fieldValue) {
+            	    $this->validateField($fieldName, $fieldValue);
+                }
+	
 		$stmt = $this->db->prepare("
 			INSERT INTO construction_stages
 			    (name, start_date, end_date, duration, durationUnit, color, externalId, status)
@@ -92,6 +158,10 @@ class ConstructionStages
 		// Check if the response with given id exists
 		$this->getSingle($id); 
 		
+		foreach ($data as $key => $value) {
+    		    $this->validateField($key, $value);
+		}
+		
 		// Prepare SQL query. Later we will concatenate $query and $fields in a SQL update request
 		$query = 'UPDATE construction_stages SET ';
 		$fields = array();
@@ -101,14 +171,6 @@ class ConstructionStages
 		{
 			if(in_array($key, $binds_values)) 
 			{
-				if ($key === 'status') 
-				{
-					$status = strtoupper($value);
-					if (!in_array($status, ['NEW', 'PLANNED', 'DELETED'])) 
-					{
-						throw new Exception('Invalid status');
-					}
-				}
 				$fields[] = $key . ' = :' . $key;
 			}
 		}
