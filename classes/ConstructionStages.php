5460af6fd9f5d440fe90fc5f9ebc3b22c0890dae
GoodLifeSeeker <vjunik88@gmail.com>
2023-03-03 13:40:43 +0300

    Task 2, post method passed test.

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -9,19 +9,22 @@ class ConstructionStages
     	{
             switch ($fieldName) {
                 
-                // Name validation
+                // Name field validation (lenght must be less than 255)
                 case 'name':
                     if (strlen($fieldValue) > 5) {
                         throw new Exception('Max chars amount is 255.');
                     }
                     break;
-                    
+                
+                // startDate field validation (must be in ISO format)    
                 case 'startDate':
 		    if (!preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $fieldValue)) {
     			throw new Exception('Invalid date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
 		    }
 		    break;
-                    
+                
+                // endDate field validation (must be either in ISO format or null)
+                // Also there should be correct format of startDate and endDate cannot be earlier than startDate    
                 case 'endDate':
                     if (!is_null($fieldValue)) {
                         $timestamp = strtotime($fieldValue);
@@ -37,13 +40,15 @@ class ConstructionStages
                         }
                     }
                     break;
-                    
+                
+                // durationUnit field validation (must be either "HOURS", "DAYS" or "WEEK")
                 case 'durationUnit':
                     if (!in_array($fieldValue, ['HOURS', 'DAYS', 'WEEKS'])) {
                         throw new Exception('Invalid duration unit. Use HOURS, DAYS, or WEEKS');
                     }
                     break;
-                    
+                
+                // color field validation (must be either in HEX format or null)     
                 case 'color':
                     if (!is_null($fieldValue)) {
                         if (!preg_match('/^#[0-9a-fA-F]{6}$/', $fieldValue)) {
@@ -51,13 +56,15 @@ class ConstructionStages
                         }
                     }
                     break;
-                    
+                
+                // externalId field validation (lenght must be less than 255 or it must be null)    
                 case 'externalId':
                     if (!is_null($fieldValue) && strlen($fieldValue) > 5) {
                         throw new Exception('External ID is too long');
                     }
                     break;
-                    
+                
+                // status field validation (must be either "NEW", "PLANNED" or "DELETED")      
                 case 'status':
                     if (!in_array($fieldValue, ['NEW', 'PLANNED', 'DELETED'])) {
                         throw new Exception('Invalid status. Use NEW, PLANNED, or DELETED');
@@ -112,8 +119,8 @@ class ConstructionStages
 
 	public function post(ConstructionStagesCreate $data)
 	{
+		// Add a validation to post method
 		$startDate = $data->startDate;
-	        var_dump($startDate);
 	        foreach ($data as $fieldName => $fieldValue) {
             	    $this->validateField($fieldName, $fieldValue, $startDate);
                 }
@@ -146,8 +153,8 @@ class ConstructionStages
 		// Create an array which binds names of parameters with the 'data' array's keys
 		$binds = array(
 			':name' => 'name',
-			':start_date' => 'startDate',
-			':end_date' => 'endDate',
+			':start_date' => 'start_date',
+			':end_date' => 'end_date',
 			':duration' => 'duration',
 			':durationUnit' => 'durationUnit',
 			':color' => 'color',
@@ -160,8 +167,21 @@ class ConstructionStages
 		// Check if the response with given id exists
 		$this->getSingle($id); 
 		
+		// Add a validation to patch method
+		/*
+		"name": "asd11",
+  		"startDate": "2021-09-10T00:00:00Z",
+  		"endDate": "2022-09-10T00:00:00Z",
+  		"duration": null,
+  		"durationUnit": "HOURS",
+  		"color": "#123456",
+  		"externalId": "12345",
+  		"status": "NEW"
+		*/
+		$startDate = $this->getSingle($id)[0]['startDate'];
+		var_dump($startDate);
 		foreach ($data as $key => $value) {
-    		    $this->validateField($key, $value);
+    		    $this->validateField($key, $value, $startDate);
 		}
 		
 		// Prepare SQL query. Later we will concatenate $query and $fields in a SQL update request
