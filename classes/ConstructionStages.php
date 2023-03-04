2a62fa447fac951e40df1a3419567c67e32de4d2
GoodLifeSeeker <vjunik88@gmail.com>
2023-03-04 22:36:11 +0300

    Task 2 is completed. The patch method has been updated, and a validation class was created and tested successfully with both the post and patch methods.

--- classes/ConstructionStages.php
+++ classes/ConstructionStages.php
@@ -3,75 +3,6 @@
 class ConstructionStages
 {
 	private $db;
-	
-	// Function which validates the data
-	private function validateField($fieldName, $fieldValue, $startDate=null)
-    	{
-            switch ($fieldName) {
-                
-                // Name field validation (lenght must be less than 255)
-                case 'name':
-                    if (strlen($fieldValue) > 5) {
-                        throw new Exception('Max chars amount is 255.');
-                    }
-                    break;
-                
-                // startDate field validation (must be in ISO format)    
-                case 'startDate':
-		    if (!preg_match('/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/', $fieldValue)) {
-    			throw new Exception('Invalid date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
-		    }
-		    break;
-                
-                // endDate field validation (must be either in ISO format or null)
-                // Also there should be correct format of startDate and endDate cannot be earlier than startDate    
-                case 'endDate':
-                    if (!is_null($fieldValue)) {
-                        $timestamp = strtotime($fieldValue);
-                        if ($timestamp === false) {
-                            throw new Exception('Invalid end_date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
-                        }
-                        $startTimestamp = strtotime($startDate);
-                        if ($startTimestamp === false) {
-                            throw new Exception('Invalid start date format. Use ISO format, e.g. 2022-12-31T14:59:00Z');
-                        }
-                        if ($timestamp < $startTimestamp) {
-                            throw new Exception('End date cannot be earlier than start date');
-                        }
-                    }
-                    break;
-                
-                // durationUnit field validation (must be either "HOURS", "DAYS" or "WEEK")
-                case 'durationUnit':
-                    if (!in_array($fieldValue, ['HOURS', 'DAYS', 'WEEKS'])) {
-                        throw new Exception('Invalid duration unit. Use HOURS, DAYS, or WEEKS');
-                    }
-                    break;
-                
-                // color field validation (must be either in HEX format or null)     
-                case 'color':
-                    if (!is_null($fieldValue)) {
-                        if (!preg_match('/^#[0-9a-fA-F]{6}$/', $fieldValue)) {
-                            throw new Exception('Invalid color format. Use HEX format, e.g. #FF0000');
-                        }
-                    }
-                    break;
-                
-                // externalId field validation (lenght must be less than 255 or it must be null)    
-                case 'externalId':
-                    if (!is_null($fieldValue) && strlen($fieldValue) > 5) {
-                        throw new Exception('External ID is too long');
-                    }
-                    break;
-                
-                // status field validation (must be either "NEW", "PLANNED" or "DELETED")      
-                case 'status':
-                    if (!in_array($fieldValue, ['NEW', 'PLANNED', 'DELETED'])) {
-                        throw new Exception('Invalid status. Use NEW, PLANNED, or DELETED');
-                    }
-                    break;
-            }
-        }
 
 	public function __construct()
 	{
@@ -119,12 +50,15 @@ class ConstructionStages
 
 	public function post(ConstructionStagesCreate $data)
 	{
-		// Add a validation to post method
-		$startDate = $data->startDate;
-	        foreach ($data as $fieldName => $fieldValue) {
-            	    $this->validateField($fieldName, $fieldValue, $startDate);
-                }
-	
+		// Validate input data
+		ConstructionStagesValidator::validateName($data->name);
+		ConstructionStagesValidator::validateStartDate($data->startDate);
+		ConstructionStagesValidator::validateEndDate($data->endDate, $data->startDate);
+		ConstructionStagesValidator::validateDurationUnit($data->durationUnit);
+		ConstructionStagesValidator::validateColor($data->color);
+		ConstructionStagesValidator::validateExternalId($data->externalId);
+		ConstructionStagesValidator::validateStatus($data->status);
+    		
 		$stmt = $this->db->prepare("
 			INSERT INTO construction_stages
 			    (name, start_date, end_date, duration, durationUnit, color, externalId, status)
@@ -149,79 +83,84 @@ class ConstructionStages
 		$id = intval($id);
 		$data = get_object_vars($data);
 		
-		
-		// Create an array which binds names of parameters with the 'data' array's keys
-		$binds = array(
-			':name' => 'name',
-			':start_date' => 'start_date',
-			':end_date' => 'end_date',
-			':duration' => 'duration',
-			':durationUnit' => 'durationUnit',
-			':color' => 'color',
-			':externalId' => 'externalId',
-			':status' => 'status'
-		);
-
-		$binds_values = array_values($binds);
-
-		// Check if the response with given id exists
-		$this->getSingle($id); 
-		
-		// Add a validation to patch method
-		/*
-		"name": "asd11",
-  		"startDate": "2021-09-10T00:00:00Z",
-  		"endDate": "2022-09-10T00:00:00Z",
-  		"duration": null,
-  		"durationUnit": "HOURS",
-  		"color": "#123456",
-  		"externalId": "12345",
-  		"status": "NEW"
-		*/
-		$startDate = $this->getSingle($id)[0]['startDate'];
-		var_dump($startDate);
+    	// Build the SQL query
+    	$query = 'UPDATE construction_stages SET ';	
+    		
+    	// Add to the $fields and $values arrays only those fields which are in $data
+    	foreach ($data as $key => $value) {
+    	
+        	// Convert startDate and endDate to start_date and end_date respectively
+        	if ($key === 'startDate') {
+            	$key = 'start_date';
+        	} elseif ($key === 'endDate') {
+            	$key = 'end_date';
+        	}
+        	$fields[] = $key . ' = :' . $key;
+        	$values[$key] = $value;
+    	}
+        /*
+        "id": 117,
+        "name": "abcdef",
+        "startDate": "2024-09-10T00:10:00Z",
+        "endDate": "2026-09-10T00:10:00Z",
+        "durationUnit": "HOURS",
+        "color": "#FFFFFF",
+        "externalId": "54321",
+        "status": "NEW"   
+        */ 	
+        var_dump($data);
 		foreach ($data as $key => $value) {
-    		    $this->validateField($key, $value, $startDate);
-		}
-		
-		// Prepare SQL query. Later we will concatenate $query and $fields in a SQL update request
-		$query = 'UPDATE construction_stages SET ';
-		$fields = array();
-
-		// Add to the $fields only those fields wich are in $data except 'status' data
-		foreach($data as $key => $value) 
-		{
-			if(in_array($key, $binds_values)) 
-			{
-				$fields[] = $key . ' = :' . $key;
-			}
-		}
-		
-		// If there are no fields to update, then rise an Exception
-		if (empty($fields)) 
-		{
-			throw new Exception('There are no fields to update');
-		}
-
-		// Upgrade our 'query' concatenating with 'fields'
-		$query .= implode(', ', $fields) . ' WHERE ID = :id';
-
-		// Create a PDO object '$stmt'
-		$stmt = $this->db->prepare($query);
-		$stmt->bindValue(':id', $id, PDO::PARAM_INT);
-
-		// Run through the array and if the current array's key is in data, we bind it to the appropriate request parameter
-		foreach ($binds as $bind => $key)
-		{
-			if (isset($data[$key]))
-			{
-				$stmt->bindValue($bind, $data[$key]);
-			}
+    		switch ($key) {
+        		case 'name':
+            		ConstructionStagesValidator::validateName($value);
+            		break;
+        		case 'startDate':
+            		ConstructionStagesValidator::validateStartDate($value);
+            		break;
+        		case 'endDate':
+        			$start_date = $this->getSingle($id)['start_date'];
+            		ConstructionStagesValidator::validateEndDate($value, $start_date);
+            		break;
+        		case 'durationUnit':
+           		    ConstructionStagesValidator::validateDurationUnit($value);
+            		break;
+        		case 'color':
+            		ConstructionStagesValidator::validateColor($value);
+            		break;
+        		case 'externalId':
+            		ConstructionStagesValidator::validateExternalId($value);
+            		break;
+        		case 'status':
+            		ConstructionStagesValidator::validateStatus($value);
+            		break;
+        		default:
+            		break;
+    		}
 		}
 
-		// execute 'stmt' request
-		$stmt->execute();
+    	// If there are no fields to update, then rise an Exception
+    	if (empty($fields)) {
+        	throw new Exception('There are no fields to update');
+    	}    	
+    	
+    	// Concatenate $query and $fields in a SQL update request
+    	$query .= implode(', ', $fields) . ' WHERE ID = :id';
 
+    	// Add the ID value to $values
+    	$values['id'] = $id;
+
+    	// Prepare the SQL query
+    	$stmt = $this->db->prepare($query);
+
+    	// Bind values to the query
+    	foreach ($values as $key => $value) {
+        	$stmt->bindValue(':' . $key, $value);
+    	}
+
+    	// Execute the query
+    	$stmt->execute();
+    	
+    	// Return the updated record
 		return $this->getSingle($id);
 	}
 
@@ -241,5 +180,4 @@ class ConstructionStages
 
 		return $this->getSingle($id);
 	}
-
 }
---
+++ classes/ConstructionStagesValidator.php
@@ -0,0 +1,67 @@
+<?php
+require_once __DIR__ . '/ConstructionStagesValidator.php';
+
+class ConstructionStagesValidator
+{
+    public static function validateName($name)
+    {
+        if (strlen($name) > 5) {
+            throw new Exception('Name is too long');
+        }
+    }
+
+    public static function validateStartDate($startDate)
+    {
+        if (!DateTime::createFromFormat(DateTime::ISO8601, $startDate)) {
+            throw new Exception('Invalid start date format');
+        }
+    }
+
+    public static function validateEndDate($endDate, $startDate)
+    {
+        if (!empty($endDate)) {
+            if (!DateTime::createFromFormat(DateTime::ISO8601, $endDate)) {
+                throw new Exception('Invalid end date format');
+            }
+
+            $startDateTime = new DateTime($startDate);
+            $endDateTime = new DateTime($endDate);
+
+            if ($startDateTime > $endDateTime) {
+                throw new Exception('End date must be later than start date');
+            }
+        }
+    }
+
+    public static function validateDurationUnit($durationUnit)
+    {
+        $allowedUnits = ['HOURS', 'DAYS', 'WEEKS'];
+
+        if (!empty($durationUnit) && !in_array($durationUnit, $allowedUnits)) {
+            throw new Exception('Invalid duration unit');
+        }
+    }
+
+    public static function validateColor($color)
+    {
+        if (!empty($color) && !preg_match('/^#[a-f0-9]{6}$/i', $color)) {
+            throw new Exception('Invalid color format');
+        }
+    }
+
+    public static function validateExternalId($externalId)
+    {
+        if (!empty($externalId) && strlen($externalId) > 5) {
+            throw new Exception('External ID is too long');
+        }
+    }
+
+    public static function validateStatus($status)
+    {
+        $allowedStatuses = ['NEW', 'PLANNED', 'DELETED'];
+
+        if (!in_array($status, $allowedStatuses)) {
+            throw new Exception('Invalid status');
+        }
+    }
+}
